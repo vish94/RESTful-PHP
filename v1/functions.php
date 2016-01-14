da9460b4e661aaa4d175a54c90c073660d25015d
vish94 <visheshahuja@gmail.com>
2016-01-14 07:47:36 +0000

    Updated v1

---
+++ v1/functions.php
@@ -0,0 +1,12 @@
+<?php
+    $books = array("java"->299, "c"->350, "php"->100);
+    
+    function get_price($find, $books) {
+        foreach($books as $book->$price) {
+            if($book == $find) {
+                return $pricel
+                break;
+            }
+        }
+    }
+?>
