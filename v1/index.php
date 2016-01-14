9980c9dbc76f7bf54be55ed449f562cf7e07a2af
vish94 <visheshahuja@gmail.com>
2016-01-14 08:10:20 +0000

    Added server.php

---
+++ v1/index.php
@@ -0,0 +1,20 @@
+<?php
+    if(isset($_POST['submit'])) {
+        $name = $_POST['bookname'];
+        $response = file_get_contents('server.php?name='.$name);
+        $response = json_decode($response);
+        echo "Status: ".$response['status'].'<br/>';
+        echo "Message: ".$response['status_message'].'<br/>';
+        echo "Price: ".$response['data'].'<br/>';
+    }
+?>
+
+<html>
+    <body>
+        <form action="index.php" method="POST">
+            <input name="bookname" type="text"></input>
+            <input type="submit" value="Submit" name="submit"></input>
+        </form>
+        <i>Hints: php, java, c</i>
+    </body>
+</html>
