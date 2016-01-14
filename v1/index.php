4d4f51bcf277d001600f2b49569502ab1ea82678
vish94 <visheshahuja@gmail.com>
2016-01-14 13:18:34 +0000

    App finally working

--- v1/index.php
+++ v1/index.php
@@ -1,8 +1,14 @@
 <?php
     if(isset($_POST['submit'])) {
         $name = $_POST['bookname'];
-        $response = file_get_contents('https://restful-php-vish94.c9users.io/RESTful-PHP/v1/server.php?name='.$name);
-        $response = json_decode($response);
+        $context = stream_context_create(array(
+    'http' => array(
+        'ignore_errors' => true
+     )
+));
+        $response = file_get_contents('https://restful-php-vish94.c9users.io/RESTful-PHP/v1/server.php?name='.$name, false, $context);
+
+        $response = json_decode($response,true);
         echo "Status: ".$response['status'].'<br/>';
         echo "Message: ".$response['status_message'].'<br/>';
         echo "Price: ".$response['data'].'<br/>';
