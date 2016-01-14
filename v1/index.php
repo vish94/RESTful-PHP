6f61a98d13edef1b89e13af45d69ee11178475bd
vish94 <visheshahuja@gmail.com>
2016-01-14 07:57:22 +0000

    Updated v1

--- v1/index.php
+++ v1/index.php
@@ -1,7 +1,7 @@
 <?php
     include('functions.php');
     //process client request (Via URL)
-    
+    header("Content-Type:application/json");
     if(!empty($_GET['name'])) {
         //process 
         $name = $_GET['name'];
@@ -14,6 +14,15 @@
         }
     } else{
         //throw error
+    }
+    
+    function deliver_response($status, $status_message, $data) {
+        header('HTTP/1.1'. $status.' '.$status_message);
+        $response['status'] = $status;
+        $response['status_message'] = $status_message;
+        $response['data'] = $data;
+        $json_response = jason_encode($response);
+        echo $json_response;
     }
     
 ?>
